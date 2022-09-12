ce1dca6f27bf99f26e9105c69924a208105620a9
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-12 15:26:00 +0100

    Update InputTodo file and css file with respective stylings

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -25,14 +25,16 @@ class InputTodo extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} className="form-container">
         <input
           type="text"
-          placeholder="Add Todo..."
+          className="input-text"
+          placeholder="Add todo..."
           value={this.state.title}
+          name="title"
           onChange={this.onChange}
         />
-        <button>Submit</button>
+        <button className="input-submit">Submit</button>
       </form>
     )
   }

15243dc46b27a51815bdaf1d66a7f5b0ad20300c
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-12 15:22:46 +0100

    Import css file to index file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom"
 
 //component file
 import TodoContainer from "./components/TodoContainer"
+
+//stylesheet
+import "./App.css"
+
 ReactDOM.render(
   <React.StrictMode>
     <TodoContainer />

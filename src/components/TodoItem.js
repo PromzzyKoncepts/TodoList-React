218a32afa930c2be8982f59e30234a3df2b1887a
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-13 09:01:16 +0100

    Update Edit task functionalities

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,16 @@ import React from "react"
 import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
+  state = {
+    editing: false,
+  }
+  
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    })
+  }
+
   render() {
     const completedStyle = {
       fontStyle: "italic",
@@ -9,20 +19,42 @@ class TodoItem extends React.Component {
       opacity: 0.4,
       textDecoration: "line-through",
     }
+
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
+
+    const { completed, id, title } = this.props.todo
     return (
       <li className={styles.item}>
-        <input
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
           type="checkbox"
           className={styles.checkbox}
-          checked={this.props.todo.completed}
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          checked={completed}
+          onChange={() => this.props.handleChangeProps(id)}
         />
-        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        <button onClick={() => this.props.deleteTodoProps(id)}>
           Delete
         </button>
-        <span style={this.props.todo.completed ? completedStyle : null}>
-          {this.props.todo.title}
+        <span style={completed ? completedStyle : null}>
+          {title}
         </span>
+        </div>
+        <input
+          type="text"
+          style={editMode}
+          className={styles.textInput}
+          value={title}
+          onChange={e => {
+            this.props.setUpdate(e.target.value, id)
+          }}
+        />
       </li>
     )
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -11,6 +11,7 @@ class TodoList extends React.Component {
             todo={todo}
             handleChangeProps={this.props.handleChangeProps}
             deleteTodoProps={this.props.deleteTodoProps}
+            setUpdate={this.props.setUpdate}
           />
           
         ))}
